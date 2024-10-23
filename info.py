e70e7c463e0e046f5ad39b2a505de2e3f468d6ab
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-10-23 13:28:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://painful-jelene-file-search-robot-b9159aa9.koyeb.app/")
+URL = environ.get("URL", "https://effective-sayre-file-search-robot-e15974cf.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
